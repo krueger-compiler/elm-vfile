b2c69bc30ff6a2411b9ae579c452d806dd949055
KruegerCompiler <KruegerCompiler@users.noreply.github.com>
2019-12-04 08:46:55 -0500

    Allow getting a hold of VFile contents

--- src/VFile.elm
+++ src/VFile.elm
@@ -1,11 +1,22 @@
-module VFile exposing (VFile, decoder, encode)
+module VFile exposing (VFile, contents, contentsAsString, decoder, encode)
 
 import Json.Decode as JD exposing (Decoder, Value)
+import VFile.Core.VFileContents as VFC exposing (VFileContents)
 import VFile.Core.VFileProperties as VFP exposing (VFileProperties)
 
 
 type VFile
     = VFile VFileProperties
+
+
+contents : VFile -> Maybe VFileContents
+contents (VFile file) =
+    file.contents
+
+
+contentsAsString : VFile -> Maybe String
+contentsAsString (VFile file) =
+    Maybe.map VFC.asString file.contents
 
 
 encode : VFile -> Value
--- src/VFile/Core/VFileContents.elm
+++ src/VFile/Core/VFileContents.elm
@@ -1,4 +1,4 @@
-module VFile.Core.VFileContents exposing (VFileContents, encode, decoder, fromString)
+module VFile.Core.VFileContents exposing (VFileContents, asString, decoder, encode, fromString)
 
 import Json.Decode as JD exposing (Decoder)
 import Json.Encode as JE exposing (Value)
@@ -6,6 +6,13 @@ import Json.Encode as JE exposing (Value)
 
 type VFileContents
     = StringContents String
+
+
+asString : VFileContents -> String
+asString vFileContents =
+    case vFileContents of
+        StringContents text ->
+            text
 
 
 fromString : String -> VFileContents
